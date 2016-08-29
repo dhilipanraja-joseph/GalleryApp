9da56d859dafcbad937049de977b630b4e1c72be
dhilipanraja-joseph <dlipnraj@gmail.com>
2016-08-29 12:22:34 -0700

    add front end render

--- src/components/ShowImages.js
+++ src/components/ShowImages.js
@@ -21,8 +21,31 @@ export default class ShowImages extends Component {
     this.setState({ images : ImageStore.getImages() });
   }
   render(){
-    return (
-      <div>ShowImages</div>
-    )
+      let imagesRow = this.state.images.map(image=>{
+          return (
+            <tr key={image._id}>
+              <td><img src={image.imgurl} width="150px" alt="NO IMAGE"/></td>
+              <td>{image.createdAt}</td>
+              <td><button onClick={this.deletePet.bind(null,image._id)}>Delete</button>
+                  </td>
+            </tr>
+          )
+      });
+      return (
+
+            <table className="table">
+              <thead>
+                <tr>
+                  <th>Image</th>
+                  <th>CreatedAt</th>
+                  <th>Options</th>
+                </tr>
+              </thead>
+              <tbody>
+                {imagesRow}
+              </tbody>
+            </table>
+
+      )
+
   }
-}
